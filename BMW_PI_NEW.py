4331f2b33e2ce6ddc8f0a64655d3eb3f6da027c5
RPM_at_18545 <rich@highlandcellars.com>
2019-06-07 07:49:03 -0700

    Start of Rewrite; Fakespeed is thread, nothing happens absent an event TODO--fix the bailout code--not reached.

--- BMW_PI_NEW.py
+++ BMW_PI_NEW.py
@@ -1,11 +1,13 @@
 
 #!/usr/bin/python3
-import pygame, sys, time, threading
+import pygame, sys, time
 from pygame.locals import *
 import pygame.gfxdraw
 from subprocess import run
 from gpiozero import LED, Button
 from time import sleep
+from threading import Thread
+
 
 
 def message_display(text, fclr=(255, 0, 0), bclr=(128, 128, 128), font=None, fsize=20, loc=(0, 0)):
@@ -26,15 +28,27 @@ def evenodd(eo):
     return eo + 1
 
 
-def fakespd(oldspd, tmint, acc):
+def fakespd(tmint):
 # replace by reading actual speed
+# thread; updates spd every tmint seconds
 # tmint is timer in ms; acc is rate to accelerate
-    secs = int((tmint/1000))    # time ins seconds
-##    print ('secs = {}'.format(secs))
-    if secs % acc == 0:
-        oldspd += 1
-    return oldspd %150
-    # return (pygame.time.get_ticks()/1000) % 150
+
+    # print ('time int = " + str(timint))
+    global spdrdg
+    
+    while True:
+        time.sleep(tmint)
+        spdrdg = (spdrdg +1) % 150
+        
+        
+def speedodisplay():
+    # uses global defined everything?
+    text1 = str(spdrdg)
+    tcol = RED
+    if spdrdg <= 55:
+        tcol = WHITE
+    message_display(text1, tcol, None, font="Arial Black", fsize=400, loc=SpeedoLoc)
+ 
 
 # GPIO setup
 ledlt= LED(17)
@@ -92,67 +106,85 @@ image  = pygame.image.load('/home/pi/Desktop/NewBkGnd.png')
 TIMEEVENT_1 = pygame.USEREVENT
 # TIME_MS = 700   # ms to update screen--this will be turn signal On/Off
 
-### inserts Time custom into the event queue @ TIME_MS
-##pygame.time.set_timer(TIMEEVENT_1, TIME_MS)
+
 is_running = True
 eo = 0
-spdrdg = 0
 
-videoPath = "/home/pi/Desktop/BMW.mov"
-omx = run(["omxplayer",videoPath])
+
+
+#videoPath = "/home/pi/Desktop/BMW.mov"
+#omx = run(["omxplayer",videoPath])
+# Uncomment above for flash video
 
 pygame.init()
+
+
 
 pygame.time.set_timer(TIMEEVENT_1, TIME_MS)
 DISPLAYSURF.blit(image, (0, 0))
 pygame.display.update()
 
-# is_running is bailout
+spdrdg = 0  # make it global
+speedupdate = 4
+t = Thread(target=fakespd, args=(speedupdate,))
+t.start()
+
+# is_running is bailout; replace by True & pygame.quit?
 while is_running:
-    # gets events from the event queue
+    # gets events from the event queue; this doesn't work in below while
     for event in pygame.event.get():
         # if the 'close' button of the window is pressed
-        if event.type == pygame.QUIT:
+        if (event.type == pygame.QUIT or event.type == K_ESCAPE):
+            pygame.quit()
+            sys.exit()
             # stops the application
-            is_running = False
-        
-        keys = pygame.key.get_pressed()
-        if keys[K_ESCAPE]:
-            # bail
-            is_running = False
-                    
+            
+#        
+#    keys = pygame.key.get_pressed()
+#    if keys[K_ESCAPE]:
+#        # bail
+#        is_running = False
+            
+# TODO fix this loop so bail out conditions are included    
+    while not (buttonlt.is_pressed or buttonrt.is_pressed):
+        # until a turn signal depressed, just update speedo
+        # and range, etc.  Other stuff that changes
+        # TODO add range, etc. update pgms here
+        DISPLAYSURF.blit(image, (0, 0))
+        speedodisplay()
+        pygame.display.update()                    
 
         # captures the custom event 'TIMEEVENT_1'
-        if event.type == TIMEEVENT_1:
-            # prints on the console that 'TIME EVENT_1' has been captured and the capture time
-            mod = pygame.time.get_ticks()
+    if event.type == TIMEEVENT_1:
+        # prints on the console that 'TIME EVENT_1' has been captured and the capture time
+        mod = pygame.time.get_ticks()
 ##            print ('custom event "TIMEEVENT_1" captured ({}) sec)'.format(mod/1000.))
-            # DISPLAYSURF.fill(GREY)
-            DISPLAYSURF.blit(image, (0, 0))
+        # DISPLAYSURF.fill(GREY)
+        DISPLAYSURF.blit(image, (0, 0))
 ##            pygame.display.update()
-        # display speed
-            spdrdg = fakespd(spdrdg,mod, 1)
-            eo = evenodd(eo)   # toggles every call
-            text1 = str(spdrdg)
-            tcol = RED
-            if spdrdg <= 55:
-                tcol = WHITE
-            message_display(text1, tcol, None, font="Arial Black", fsize=400, loc=SpeedoLoc)
-            if eo==1 and buttonlt.is_pressed:
-                pygame.draw.polygon(DISPLAYSURF, GREEN, (
-                    (ArrowLtXPos, (ArrowYPos + ArrowH / 2)), ((ArrowLtXPos - ArrowW), ArrowYPos),
-                    (ArrowLtXPos, (ArrowYPos - ArrowH / 2))))
-                ledlt.on()
-            if eo==1 and buttonrt.is_pressed:
-                pygame.draw.polygon(DISPLAYSURF, GREEN, (
-                    (ArrowRtXPos, (ArrowYPos + ArrowH / 2)), ((ArrowRtXPos + ArrowW), ArrowYPos),
-                    (ArrowRtXPos, (ArrowYPos - ArrowH / 2))))
-                ledrt.on()    
-            if eo==0:
-                ledlt.off()
-                ledrt.off()
-                    
-            pygame.display.update()
+    # display speed
+        spdrdg = fakespd(spdrdg,mod, 1)
+        eo = evenodd(eo)   # toggles every call
+        text1 = str(spdrdg)
+        tcol = RED
+        if spdrdg <= 55:
+            tcol = WHITE
+        message_display(text1, tcol, None, font="Arial Black", fsize=400, loc=SpeedoLoc)
+        if eo==1 and buttonlt.is_pressed:
+            pygame.draw.polygon(DISPLAYSURF, GREEN, (
+                (ArrowLtXPos, (ArrowYPos + ArrowH / 2)), ((ArrowLtXPos - ArrowW), ArrowYPos),
+                (ArrowLtXPos, (ArrowYPos - ArrowH / 2))))
+            ledlt.on()
+        if eo==1 and buttonrt.is_pressed:
+            pygame.draw.polygon(DISPLAYSURF, GREEN, (
+                (ArrowRtXPos, (ArrowYPos + ArrowH / 2)), ((ArrowRtXPos + ArrowW), ArrowYPos),
+                (ArrowRtXPos, (ArrowYPos - ArrowH / 2))))
+            ledrt.on()    
+        if eo==0:
+            ledlt.off()
+            ledrt.off()
+                
+        pygame.display.update()
 
 
 
