2092065969bd3145c81b64c5b73c3581860051ce
rich <rich@highlandcellars.com>
2019-06-09 08:41:44 -0700

    Bunch of rewrites; not debugged.  Event set up TODO

--- BMW_PI_NEW.py
+++ BMW_PI_NEW.py
@@ -7,6 +7,7 @@ from subprocess import run
 from gpiozero import LED, Button
 from time import sleep
 from threading import Thread
+from colorzero import *
 
 
 def message_display(text, fclr=(255, 0, 0), bclr=(128, 128, 128), font=None, fsize=20, loc=(0, 0)):
@@ -40,10 +41,28 @@ def speedodisplay():
     if spdrdg <= 55:
         tcol = WHITE
     message_display(text1, tcol, None, font="Arial Black", fsize=400, loc=SpeedoLoc)
- 
+
+def ck_action():
+    # builds a list of actions
+    # button pushes, quit, ESC key
+    lr = []     #start null
+    if buttonlt.is_pressed:
+        lr.append("LEFT")
+    if buttonrt.is_pressed:
+        lr.append("RIGHT")
+    # gets events from the event queue; this doesn't work in below while
+    for event in pygame.event.get():
+        # if the 'close' button of the window is pressed
+        if event.type == pygame.QUIT:
+            lr.append("QUIT")
+        if event.type == K_ESCAPE:
+            lr.append("ESC")
+
+    return lr
+
 
 # GPIO setup
-ledlt= LED(17)
+ledlt = LED(17)
 ledrt = LED(27)
 buttonlt = Button(10)
 buttonrt = Button(22)
@@ -78,6 +97,7 @@ ChRectH = int(0.05 * dis_h)
 ChgTextLoc = (int(0.5 *dis_w ), (ChgRectY + ChRectH + int(0.05*dis_h) ))
 
 # set up the colors  ?? Put a palette in place
+# TODO--why not use colorzero colors?
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
@@ -94,7 +114,6 @@ image  = pygame.image.load('/home/pi/Desktop/NewBkGnd.png')
 
 
 # set up custom time event
-
 TIMEEVENT_1 = pygame.USEREVENT
 # TIME_MS = 700   # ms to update screen--this will be turn signal On/Off
 
@@ -102,15 +121,11 @@ TIMEEVENT_1 = pygame.USEREVENT
 is_running = True
 eo = 0
 
-
-
-#videoPath = "/home/pi/Desktop/BMW.mov"
-#omx = run(["omxplayer",videoPath])
+# videoPath = "/home/pi/Desktop/BMW.mov"
+# omx = run(["omxplayer",videoPath])
 # Uncomment above for flash video
 
 pygame.init()
-
-
 
 pygame.time.set_timer(TIMEEVENT_1, TIME_MS)
 DISPLAYSURF.blit(image, (0, 0))
@@ -118,42 +133,43 @@ pygame.display.update()
 
 spdrdg = 0  # make it global
 speedupdate = 4
+
 t = Thread(target=fakespd, args=(speedupdate,))
 t.start()
 
 # is_running is bailout; replace by True & pygame.quit?
 while is_running:
     # gets events from the event queue; this doesn't work in below while
-    for event in pygame.event.get():
-        # if the 'close' button of the window is pressed
-        if (event.type == pygame.QUIT or event.type == K_ESCAPE):
-            pygame.quit()
-            sys.exit()
-            # stops the application
-            
-#        
-#    keys = pygame.key.get_pressed()
-#    if keys[K_ESCAPE]:
-#        # bail
-#        is_running = False
-            
-# TODO fix this loop so bail out conditions are included    
-    while not (buttonlt.is_pressed or buttonrt.is_pressed):
+    # for event in pygame.event.get():
+    #     # if the 'close' button of the window is pressed
+    #     if (event.type == pygame.QUIT or event.type == K_ESCAPE):
+    #         pygame.quit()
+    #         sys.exit()
+    #         # stops the application
+
+    # TODO fix this loop so bail out conditions are included
+    ck_list = []    # don't call ck_action repeatedly.
+    while len(ck_list) == 0:
+        ck_list = ck_action()
         # until a turn signal depressed, just update speedo
         # and range, etc.  Other stuff that changes
         # TODO add range, etc. update pgms here
         DISPLAYSURF.blit(image, (0, 0))
         speedodisplay()
-        pygame.display.update()                    
+        pygame.display.update()
+    if "QUIT" in ck_list or "ESC" in ck_list:   # Or any other bail outs
+        pygame.quit()
+        sys.exit()
+    if "LEFT" in ck_list:
+
 
         # captures the custom event 'TIMEEVENT_1'
     if event.type == TIMEEVENT_1:
         # prints on the console that 'TIME EVENT_1' has been captured and the capture time
         mod = pygame.time.get_ticks()
-##            print ('custom event "TIMEEVENT_1" captured ({}) sec)'.format(mod/1000.))
-        # DISPLAYSURF.fill(GREY)
+#        print ('custom event "TIMEEVENT_1" captured ({}) sec)'.format(mod/1000.))
         DISPLAYSURF.blit(image, (0, 0))
-##            pygame.display.update()
+#
     # display speed
         spdrdg = fakespd(spdrdg,mod, 1)
         eo = evenodd(eo)   # toggles every call
